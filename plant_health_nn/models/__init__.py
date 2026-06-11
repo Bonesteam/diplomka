4311960ccbc210e41baf02799e3e8cb1e1ac6a2b
Kostia Sidorchuk <kostyayarmolintsi2004@gmail.com>
2026-06-11 20:11:59 +0300

    видалення дублікатного файла

--- plant_health_nn/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .mlp_model import build_mlp
-from .cnn_model import build_cnn
-from .trainer import train_model
-from .predictor import get_probabilities, load_model
-from .baseline_ml import train_baselines
--- plant_health_nn/preprocessing/__init__.py
+++
@@ -1,3 +0,0 @@
-from .loader import load_data, FEATURE_COLS, TARGET_COL, CLASS_NAMES
-from .scaler import fit_transform, transform, load_scaler
-from .splitter import split_from_config, format_split_summary, get_split_description
